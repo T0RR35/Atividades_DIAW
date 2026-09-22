a553bf9a64a573f240f1ed0fe5ec173f8f9ba4c3
Rafael Torres <rafaelmodestoft@gmail.com>
2026-09-22 13:53:19 -0300

    Add FIPE API again

---
+++ Fipe_RestAPI/src/test/java/com/example/FipeAPI/FipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FipeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes={com.example.FipeAPI.application.FipeApiApplication.class})
+class FipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
